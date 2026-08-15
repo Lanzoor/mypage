e348d5426447fee067707725c8c762ac877109b9
Lanzoor <lanzoor-programming@proton.me>
2026-08-15 15:20:30 +0900

    core :: improve +error.svelte design

--- src/routes/(main)/+error.svelte
+++ src/routes/(main)/+error.svelte
@@ -11,35 +11,25 @@
 
 {#if status === 404}
     <section id="error-404" class="stretched">
+        <a href="/" onclick={() => history.back()}>
+            {"<-"} Back
+        </a>
+
         <header>
-            <h1>404 - Page not found!</h1>
-            <p>
-                <b>
-                    We couldn't find a resource located at
-                    <span class="col bright green">'{attemptedPath}'</span>.
-                </b><br />
-                The filename may have changed, or the file may have been moved or
-                removed.
-            </p>
+            <h1>404</h1>
+
+            <div class="separator"></div>
+
+            <h2>Page not found</h2>
         </header>
 
-        <div>
-            <p>
-                <b>If you are a visitor:</b><br />
-                You can try searching for pages with similar names.<br />
-                Please use the navigation panels or the buttons below.
-            </p>
-        </div>
-
-        <div id="buttons">
-            <a class="plain" href="/" onclick={() => history.back()}>
-                <button>← Back</button>
-            </a>
-            <a class="plain" href="/">
-                <button>Home</button>
-            </a>
-            <a href={attemptedPath}>↩ Try again</a>
-        </div>
+        <p>
+            <b>
+                We couldn't find a resource located at
+                <span class="col bright green">'{attemptedPath}'</span>.
+            </b><br />
+            The filename may have changed, or the file may have been moved or removed.
+        </p>
     </section>
     <!-- {:else}
     <section class="stretched">
@@ -50,19 +40,32 @@
 
 <style lang="css">
     #error-404 {
-        background:
-            radial-gradient(
-                circle at left top,
-                rgba(100, 0, 255, 0.5),
-                transparent 50%
-            ),
-            #111;
+        background: radial-gradient(
+            circle at 50% 50%,
+            rgba(100, 0, 255, 0.25),
+            transparent
+        );
     }
 
     #error-404 header {
-        align-self: center;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1.5em;
+    }
+
+    #error-404 header .separator {
+        width: 2px;
+        height: 2em;
+
+        background: rgba(255, 255, 255, 0.25);
+    }
+
+    #error-404 p {
         text-align: center;
     }
+
     #error-404 #buttons {
         display: flex;
         flex-direction: row;
